f750b6285f2757b0da99a3202bedafac69ea820e
Karel Bemelmans <Karel.Bemelmans@UNIBET.com>
2016-11-10 16:59:19 +0100

    Fixed GO_SERVER_URL overwriting logic

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -34,8 +34,10 @@ log4j.appender.ConsoleAppender.layout.ConversionPattern=%d{ISO8601} [%-9t] %-5p 
 EOL
 
 # update config to point to correct go.cd server hostname and port
-if [ -n "$GO_SERVER_URL" ]; then
+if [ ! -n "$GO_SERVER_URL" ]; then
   sed -i -e "s|GO_SERVER_URL=https://127.0.0.1:8154/go|GO_SERVER_URL=${GO_SERVER_URL}|" /etc/default/go-agent
+else
+  GO_SERVER_URL="https://127.0.0.1:8154/go"
 fi
 
 # wait for server to be available
