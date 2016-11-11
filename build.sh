1926aac0f149fbd74d9c09da9e5602b3abcd6a72
Karel Bemelmans <Karel.Bemelmans@UNIBET.com>
2016-11-11 14:36:02 +0100

    Fixed build script to properly tag the latest image

--- build.sh
+++ build.sh
@@ -5,17 +5,20 @@
 # - DOCKER_HUB_USERNAME
 # - DOCKER_HUB_PASSWORD
 
-ECR_IMAGE_NAME="unibet/gocd-agent"
-ECR_IMAGE_TAG="16.11.0"
+IMAGE_NAME="unibet/gocd-agent"
+IMAGE_TAG="16.11.0"
 
 # Login to ECR and pull the latest version of the base image
 $(aws ecr get-login --region us-west-2 --registry-ids 137112412989)
 docker pull 137112412989.dkr.ecr.us-west-2.amazonaws.com/amazonlinux:latest
 
 # 1. Build the production container.
-docker build -t ${ECR_IMAGE_NAME}:${ECR_IMAGE_TAG} .
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
 
 # 2. Push it to Docker Hub
 docker login --username ${DOCKER_HUB_USERNAME} --password ${DOCKER_HUB_PASSWORD}
-docker push ${ECR_IMAGE_NAME}:${ECR_IMAGE_TAG}
-docker push ${ECR_IMAGE_NAME}:latest
+
+docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_NAME}:latest
+
+docker push ${IMAGE_NAME}:${IMAGE_TAG}
+docker push ${IMAGE_NAME}:latest
